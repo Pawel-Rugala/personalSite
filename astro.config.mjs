68dac3053aee4566d9035c89d0f755e1430e2798
Pawel Rugala <webdevpablo@gmail.com>
2022-09-16 19:11:39 +0200

    init

---
+++ astro.config.mjs
@@ -0,0 +1,16 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    ssr: {
+      external: ['svgo']
+    }
+  },
+  output: "server",
+  adapter: cloudflare()
+});
