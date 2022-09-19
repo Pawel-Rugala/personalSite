50701a9864ebc543948a8f0c686170f25140fea2
Pawel Rugala <webdevpablo@gmail.com>
2022-09-19 21:16:21 +0200

    FIX: Replace astro-icon with Iconify

---
+++ src/components/Icon.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { Icon } from '@iconify/react'
+
+export default function Icons({ icon, height, className }) {
+ return <Icon icon={icon} height={height} className={className} />
+}
