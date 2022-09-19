af8397a77d014209d0136f9b42f1065b49d71b67
Pawel Rugala <webdevpablo@gmail.com>
2022-09-19 18:14:56 +0200

    FIX: Cloudflare astro-icon issue

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,11 +7,6 @@ import react from '@astrojs/react'
 // https://astro.build/config
 export default defineConfig({
  integrations: [tailwind(), react()],
- vite: {
-  ssr: {
-   external: ['svgo'],
-  },
- },
  output: 'server',
  adapter: cloudflare(),
 })
