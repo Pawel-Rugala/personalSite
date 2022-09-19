43e179ea551bd1ba6d45d4bd755fd88f477d2c2d
Pawel Rugala <webdevpablo@gmail.com>
2022-09-19 16:51:49 +0200

    FIX: svgo build process

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import cloudflare from "@astrojs/cloudflare";
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import cloudflare from '@astrojs/cloudflare'
 
-import react from "@astrojs/react";
+import react from '@astrojs/react'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()],
-  vite: {
-    ssr: {
-      external: ['svgo']
-    }
+ integrations: [tailwind(), react()],
+ vite: {
+  ssr: {
+   external: ['svgo'],
   },
-  output: "server",
-  adapter: cloudflare()
-});
+ },
+ output: 'server',
+ adapter: cloudflare(),
+})
--- src/pages/blog/hello-world.md
+++ src/pages/blog/hello-world.md
@@ -1,4 +1,9 @@
 ---
+layout: ../../layouts/Blog.astro
+title: 'Hello world'
+description: 'This is a blog post'
+author: 'John Doe'
+date: '2020-01-01'
 ---
 
 # Hello World
